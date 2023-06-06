449b2eb49d2ca8f9e6b8258b498d8335c5c1623b
Golang Uzbekistan <golanguzb70@gmail.com>
2023-06-06 06:46:29 +0500

    add template handler to interface of storage

--- storage/storage.go
+++ storage/storage.go
@@ -4,15 +4,24 @@ import (
 	"github.com/golanguzb70/go-gin-basicauth-postgres-monolithic-template/config"
 	"github.com/golanguzb70/go-gin-basicauth-postgres-monolithic-template/pkg/db"
 	"github.com/golanguzb70/go-gin-basicauth-postgres-monolithic-template/pkg/logger"
+	"github.com/golanguzb70/go-gin-basicauth-postgres-monolithic-template/storage/postgres/templaterepo"
 )
 
 type StorageI interface {
+	Template() templaterepo.TemplateI
 }
 
 type StoragePg struct {
+	template templaterepo.TemplateI
 }
 
 // NewStoragePg
 func New(db *db.Postgres, log *logger.Logger, cfg *config.Config) StorageI {
-	return &StoragePg{}
+	return &StoragePg{
+		template: templaterepo.New(db, log, cfg),
+	}
+}
+
+func (s *StoragePg) Template() templaterepo.TemplateI {
+	return s.template
 }
